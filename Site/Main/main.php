0965ac3950c0d1db11195fc79bef82da56101f36
solnoganu <dattthic1@dattthic1gmail,thic1gmailcom>
2024-11-13 13:09:10 +0700

    first commit

---
+++ Site/Main/main.php
@@ -0,0 +1,39 @@
+<div class="section container">
+    <!-- ============================ Commercial ============================ -->
+    <div class="section_1 space-container">
+        <!-- ---------------- Introduce ---------------- -->
+        <div class="title_introduction " style="text-align:center">
+            <h3> Move, Shop, Customise & Celebrate With Us.</h3>
+            <span>
+                No matter what you feel like doing today, It’s better as a Member.
+            </span>
+            <br>
+            <a href="../Main/index.php?signup" style="text-decoration:underline;color: black">Join Us</a>
+        </div>
+
+        <!-- ---------------- Video ---------------- -->
+        <div class="commercial_dune">
+            <video class="container" autoplay loop muted controls="false">
+                <source src="<?= $CONTENT_URL; ?>/Images/poster/commercial_dune.mp4" type="video/mp4">
+            </video>
+        </div>
+
+        <!-- ---------------- Title ---------------- -->
+        <div class="commercial_title ">
+            <h1 class="font-container">TODAY JUST GOT EASIER</h1>
+            <p>Find You Passion</p>
+            <button class="button-shop">Shop</button>
+        </div>
+    </div>
+
+    <!-- ============================ Select Product By Sport ============================ -->
+    <div class="section_2 space-container">
+        <!-- ---------------- Title ---------------- -->
+        <h1>Shop By Sport</h1>
+
+        <!-- ---------------- Sport Product ---------------- -->
+        <div class="selection_sport">
+
+        </div>
+    </div>
+</div>
